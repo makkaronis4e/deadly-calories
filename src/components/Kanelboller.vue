2fde9f61adc955cbd5a3bf1596cb7120280a3b47
dmytrosych <d.sych@relevant.software>
2024-03-20 00:59:01 +0100

    Create welcome page

---
+++ src/components/Kanelboller.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+      class="kanelboller-image"
+      :style="componentStyles"
+      src="../assets/img/kanelboller.png"
+      alt="Kanelboller">
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: 'Kanelboller',
+    props: {
+      size: {
+        type: String,
+        default: '20',
+      },
+    },
+    computed: {
+      componentStyles() {
+        return {
+          '--image-height': this.size + 'px',
+        };
+      }
+    }
+  });
+</script>
+
+<style scoped lang="scss">
+  .kanelboller-image {
+    height: var(--image-height);
+  }
+</style>
